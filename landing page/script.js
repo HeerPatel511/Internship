bb2abaf4425e2cade3803939940e1a858b7fb614
HeerPatel511 <heerd511@gmail.com>
2025-03-30 19:31:53 +0530

    Add files via upload

---
+++ landing page/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.get-started').addEventListener('click', () => {
+    alert('Ready to start your app journey!');
+});
+
+document.querySelector('.walk-through').addEventListener('click', () => {
+    alert('Let’s walk through your startup idea step by step!');
+});
